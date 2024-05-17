2fb7fe51eac9a31cb4093f56465863c0f229f743
alifoo <alissonayres.trb@gmail.com>
2024-05-16 23:02:42 -0300

    created function to normalize urls and added some tests using jest

--- crawl.js
+++ crawl.js
@@ -1,5 +1,28 @@
 function normalizeURL(url) {
-    return 1
+    const prefix1 = "http://";
+    const prefix2 = "https://";
+    if (typeof url !== 'string' || (!url.startsWith(prefix1) && !url.startsWith(prefix2))) {
+        throw new Error("Invalid input");
+    };
+
+    let normalizedURL;
+    
+    if (url.startsWith(prefix1)) {
+        normalizedURL = url.replace(prefix1, '');
+        if (normalizedURL.endsWith('/')){
+            return normalizedURL.slice(0, -1);
+        }
+        return normalizedURL;
+    };
+
+    if (url.startsWith(prefix2)) {
+        normalizedURL = url.replace(prefix2, '');
+        if (normalizedURL.endsWith('/')){
+            return normalizedURL.slice(0, -1);
+        }
+        return normalizedURL;
+    };
 }
+
 
 export { normalizeURL };
--- crawl.test.js
+++ crawl.test.js
@@ -1,1 +1,14 @@
+import { normalizeURL } from "./crawl.js";
 import { test, expect } from "@jest/globals";
+
+test('normalizeURL should return the normalized URL', () => {
+    const inputURL = 'http://blog.boot.dev/path/';
+    const expectedOutput = "blog.boot.dev/path";
+    expect(normalizeURL(inputURL)).toBe(expectedOutput);
+});
+
+test('throws an error with invalid input', () => {
+    expect(() => {
+        normalizeURL(23425463);
+    }).toThrow("Invalid input");
+});
